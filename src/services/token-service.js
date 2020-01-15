a3e9f21addcb1ecdcc840caf85d84bc3b2c6130c
Zee Celest <zeecelest@gmail.com>
2020-01-15 12:31:05 -0500

    finishing up

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -19,3 +19,24 @@ const TokenService = {
 }
 
 export default TokenService
+import config from '../config'
+
+const TokenService = {
+  saveAuthToken(token) {
+    window.localStorage.setItem(config.TOKEN_KEY, token)
+  },
+  getAuthToken() {
+    return window.localStorage.getItem(config.TOKEN_KEY)
+  },
+  clearAuthToken() {
+    window.localStorage.removeItem(config.TOKEN_KEY)
+  },
+  hasAuthToken() {
+    return !!TokenService.getAuthToken()
+  },
+  makeBasicAuthToken(userName, password) {
+    return window.btoa(`${userName}:${password}`)
+  },
+}
+
+export default TokenService
